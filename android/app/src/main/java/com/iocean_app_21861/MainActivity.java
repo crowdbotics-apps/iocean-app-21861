2ee9e5465eb82da0653d63e07e77620225dc39c2
Crowdbotics <team@crowdbotics.com>
2020-10-23 06:21:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/iocean_app_21861/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iocean_app_21861;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "iocean_app_21861";
+    }
+}
